52a2c9b77a1283dc917b2e2855e86a4fdd2cee54
suhascv <suhas749@protonmail.com>
2021-05-10 18:02:46 -0400

    path update

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
+const path = process.env.PUBLIC_URL || "/tereda-pustaka"
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={}>
       <App />
     </BrowserRouter>
   </StrictMode>,
